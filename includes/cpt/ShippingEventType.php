1e503b8a0611059a29fc1fa3830713d2b8c18f7e
Filipe Xavier <filipexts@gmail.com>
2020-08-28 20:30:27 +0000

    Add a new tag to show specific content when orders are available or not on the shortcode

--- includes/cpt/ShippingEventType.php
+++ includes/cpt/ShippingEventType.php
@@ -14,7 +14,9 @@ class ShippingEventType {
     '#EVENT_DATE#' => 'Use this tag anywhere and the user will see the delivery/pickup date of each Shipping Event',
     '#BEGIN_ORDERS_DATE#' => 'Use this tag anywhere and the user will see when the orders are opened for each Shipping Event',
     '#END_ORDERS_DATE#' => 'Use this tag anywhere and the user will see the last day before orders are closed for each Shipping Event',
-    '#SELECT_EVENT#' => 'Put this tag in the link of a button where you want the user to select the Shipping Event and access the Store'
+    '#SELECT_EVENT#' => 'Put this tag in the link of a button where you want the user to select the Shipping Event and access the Store',
+    '#ONLY_AVAILABLE#' => 'Put this tag as a class of an element you want the user to view only when orders for this Shipping Event are available',
+    '#ONLY_UNAVAILABLE#' => 'Put this tag as a class of an element you want the user to view only when orders for this Shipping Event are NOT available'
   );
 
   public const DATE_TAGS = array(
@@ -39,7 +41,9 @@ class ShippingEventType {
       self::customize_date_format( $shipping_event->get_shipping_date(), 'wcse_event_type_event_date_format' ),
       self::customize_date_format( $shipping_event->get_begin_order_date(), 'wcse_event_type_open_orders_format' ),
       self::customize_date_format( $shipping_event->get_end_order_date(), 'wcse_event_type_close_orders_format' ),
-      self::handle_select_event_button_behavior( $shipping_event )
+      self::handle_select_event_button_behavior( $shipping_event ),
+      self::handle_availability_class( $shipping_event->orders_enabled() ),
+      self::handle_availability_class( !$shipping_event->orders_enabled() )
     );
 
     return str_replace( self::get_event_tags(), $new_str_keys, $original_text );
@@ -48,6 +52,10 @@ class ShippingEventType {
   public static function handle_select_event_button_behavior( $shipping_event ) {
     if( $shipping_event->open_order_pending() ) return self::ORDERS_CLOSED_CODE;
     return self::SHIPPING_ID_CODE . $shipping_event->get_id();
+  }
+
+  public static function handle_availability_class( $show_text ) {
+    return $show_text ? '' : 'hidden';
   }
 
   public static function customize_date_format( $date, $option_key ) {
